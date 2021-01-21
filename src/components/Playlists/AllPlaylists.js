8c6ecefe4d6b8a8f5b6d46bd2a3f74942a0cafbd
Cadell Singh <cadellsingh9@gmail.com>
2021-01-21 13:10:05 -0400

    see desc
    component holds:
    user playlists &
    global playlists
    still have to create global playlists

--- src/components/Playlists/AllPlaylists.js
+++ src/components/Playlists/AllPlaylists.js
@@ -1,9 +1,38 @@
-import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { ContainerBackgroundColor } from "../../styles/sharedStyles";
+import UserPlaylists from "./UserPlaylists";
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 15px;
+`;
+
+const PlaylistsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 15px;
+  border-radius: 10px;
+  grid-gap: 15px;
+  ${ContainerBackgroundColor}
+
+  & h2 {
+    grid-column: span 2 / auto;
+  }
+`;
 
 const AllPlaylists = () => {
   return (
     <div>
-      <p>All Playlists</p>
+      <Main>
+        <PlaylistsContainer>
+          <h2>Your Playlists</h2>
+          <UserPlaylists />
+        </PlaylistsContainer>
+        <PlaylistsContainer>
+          <h2>Global Top Playlists</h2>
+        </PlaylistsContainer>
+      </Main>
     </div>
   );
 };
