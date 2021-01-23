fec5dd54617dd7cfc878dd28af565197438d5f04
Cadell Singh <cadellsingh9@gmail.com>
2021-01-22 20:17:59 -0400

    removed cookie to log out user

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,20 +1,29 @@
 import styled from "styled-components";
 import { ContainerBackgroundColor } from "../styles/sharedStyles";
+import { useHistory } from "react-router-dom";
 
-const LogoutButton = styled.div`
+const LogoutButton = styled.button`
   padding: 20px;
   border-radius: 10px;
   width: 100%;
   margin-top: 15px;
   ${ContainerBackgroundColor};
+  cursor: pointer;
+
+  :hover {
+    border: 1px solid #05b075;
+  }
 `;
 
 const Logout = () => {
-  return (
-    <LogoutButton>
-      <p>Logout</p>
-    </LogoutButton>
-  );
+  const history = useHistory();
+
+  const handleClick = () => {
+    window.localStorage.removeItem("expiry_time");
+    return history.push("/");
+  };
+
+  return <LogoutButton onClick={handleClick}>Logout</LogoutButton>;
 };
 
 export default Logout;
