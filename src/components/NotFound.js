8897f1a6c78eb5f8b2e5f78068966ce397189c5d
Cadell Singh <cadellsingh9@gmail.com>
2021-02-01 07:50:08 -0400

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,38 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { TextColor } from "../styles/sharedStyles";
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+
+  & a {
+    font-size: 50px;
+    ${TextColor};
+  }
+
+  & a:hover {
+    text-decoration: underline;
+  }
+`;
 
 const NotFound = () => {
   return (
-    <div>
+    <Container
+      data-aos="fade-down"
+      data-aos-duration="1500"
+      data-aos-easing="ease-in-out"
+    >
       <p>
         Um, you look a bit lost... lets get you back{" "}
         <span>
-          <Link to="/">Home</Link>
+          <Link to="/">home</Link>
         </span>
       </p>
-    </div>
+    </Container>
   );
 };
 
